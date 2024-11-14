5e4ba2c853448cc4e8505cdca807efcfb25f3b86
Ivan Baha <ivan.baha@altran.com>
2024-11-14 14:51:10 +0000

    updated results logging

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import { fetchPackageJson } from "./utils/gitlab-connector.util";
 import { logger } from "./utils/logger.util";
 
 (async function () {
-  for (const { id, name } of AppConfig.projectsToCheck) {
+  const { projectsToCheck, packagesToFind } = AppConfig;
+  const total = { hits: 0, projects: 0 };
+
+  logger.info(
+    `We are about to check ${projectsToCheck.length} projects to find ${packagesToFind.length} dependencies. Grab a coffee, take a sit, and relax ☕️`
+  );
+
+  for (const { id, name } of projectsToCheck) {
     logger.info(`Checking the project '${name}'`);
 
     const packageJson = await fetchPackageJson(id);
@@ -11,15 +18,28 @@ import { logger } from "./utils/logger.util";
     if (packageJson?.dependencies) {
       const hits: Record<string, string> = {};
 
-      for (const { name, version } of AppConfig.packagesToFind) {
+      for (const { name, version } of packagesToFind) {
         if (packageJson.dependencies[name] === version) {
           hits[name] = version;
+          total.hits++;
         }
       }
 
-      if (Object.keys(hits)) logger.warn(`Found ${Object.keys(hits).length} matches:\n`);
+      if (Object.keys(hits).length) {
+        total.projects++;
+
+        logger.warn(
+          `Found ${Object.keys(hits).length} matches in "${name}" project:\n - ${Object.entries(hits)
+            .map(([k, v]) => `${k}: ${v}`)
+            .join("\n - ")}`
+        );
+      }
     }
   }
 
-  logger.success(`Successfully finished dependencies checking.`);
+  logger.success(
+    `Successfully finished dependencies checking.${
+      total.hits ? ` Found ${total.hits} matches across ${total.projects} projects.` : ""
+    }`
+  );
 })();
